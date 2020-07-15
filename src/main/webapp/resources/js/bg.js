1f7920508d060193434180a580a2fc2bc0afd61e
Simon <ghdud0503@gmail.com>
2020-07-15 15:22:10 +0900

    2020-07-15 메인 로그인화면 구현 완료 '프론트만'

--- src/main/webapp/resources/js/bg.js
+++ src/main/webapp/resources/js/bg.js
@@ -3,15 +3,11 @@ const body = document.querySelector("body");
 const IMG_NUM = 3;
 
 function paintImg(imgNumber) {
-  const video = document.createElement("video");
-  video.setAttribute("muted", "muted");
-  video.setAttribute("autoplay", "autoplay");
-  video.setAttribute("loop", "loop");
+  const video = document.getElementById('videoP')
   const source = document.createElement("source");
   source.src = `/yeol-gong/video/bg_${imgNumber}.mp4`;
   source.type = "video/mp4";
   video.appendChild(source);
-  body.prepend(video);
 }
 
 function genRandom() {
