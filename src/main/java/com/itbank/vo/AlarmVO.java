f781ab910749b30141eef6b320dd6baf7842cb56
sseung12 <pon02129@naver.com>
2020-08-19 13:31:42 +0900

    노

---
+++ src/main/java/com/itbank/vo/AlarmVO.java
@@ -0,0 +1,44 @@
+package com.itbank.vo;
+
+public class AlarmVO {
+
+	
+	private int receiver;
+
+	private String movePage;
+	private String context;
+	private String time;
+	private int readChk;
+	
+	
+	public int getReceiver() {
+		return receiver;
+	}
+	public void setReceiver(int receiver) {
+		this.receiver = receiver;
+	}
+	public String getMovePage() {
+		return movePage;
+	}
+	public void setMovePage(String movePage) {
+		this.movePage = movePage;
+	}
+	public String getContext() {
+		return context;
+	}
+	public void setContext(String context) {
+		this.context = context;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+	public int getReadChk() {
+		return readChk;
+	}
+	public void setReadChk(int readChk) {
+		this.readChk = readChk;
+	}
+}
